fe57d1434a3b83d35e58e566c7bf5798f5b2bea0
sd <sdebaun74@gmail.com>
2019-01-24 13:33:20 -0800

    use react-hookstore instead of my own hacky store

--- src/useAuthToken.ts
+++ src/useAuthToken.ts
@@ -1,36 +1,13 @@
 import React, { useState, useEffect } from 'react'
+//@ts-ignore
+import { createStore, useStore } from 'react-hookstore'
 
-// this clever useStore stuff courtesy of:
-// https://blog.usejournal.com/global-state-management-with-react-hooks-5e453468c5bf
-type Setter<T> = (val: T | null) => void
-
-type Store<T> = {
-  state: T,
-  setState: (value: T | null) => void,
-  setters: Setter<T>[]
-}
-
-const store: Store<string> = {
-  state: '',
-  setState(value: any) {
-    this.state = value;
-    this.setters.forEach(setter => setter(this.state))
-  },
-  setters: []
-}
-
-store.setState = store.setState.bind(store)
-
-const useStore = (initialState: any):[string | null, Setter<string>] => {
-  const [state, set] = useState<string | null>(initialState)
-  if (!store.setters.includes(set)) { store.setters.push(set) }
-  return [ state, store.setState ]
-}
+const authStore = createStore('authStore', localStorage['authToken'])
 
 export const useAuthToken = () => {
   const key = 'authToken'
   // const [authToken, setAuthTokenState] = useState(localStorage[key])
-  const [authToken, setAuthTokenState] = useStore(localStorage[key])
+  const [authToken, setAuthTokenState] = useStore(authStore)
 
   // writing to auth token local storage will NOT automatically update authToken via sync fn
   // https://stackoverflow.com/questions/5370784/localstorage-eventlistener-is-not-called
@@ -61,3 +38,4 @@ export const useAuthToken = () => {
     clearAuthToken,
   }
 }
+
